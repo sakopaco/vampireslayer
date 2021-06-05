1791b4c5e27cfd773051519a910ec45bca00fd32
Paco Gálvez <correopaco2000@gmail.com>
2021-06-05 19:33:40 +0200

    actualización diseño

--- doc/disenoprograma.mm
+++ doc/disenoprograma.mm
@@ -3,7 +3,7 @@
 <node CREATED="1622145402544" ID="ID_704344221" MODIFIED="1622145411431" TEXT="programa ASM">
 <node CREATED="1622145444971" ID="ID_1942422980" MODIFIED="1622145488820" POSITION="left" TEXT="Constantes BIOS"/>
 <node CREATED="1622145466473" ID="ID_665166448" MODIFIED="1622145480153" POSITION="left" TEXT="Constantes archivo bin"/>
-<node CREATED="1622145503371" FOLDED="true" ID="ID_4534354" MODIFIED="1622147260174" POSITION="left" TEXT="Constantes programa">
+<node CREATED="1622145503371" ID="ID_4534354" MODIFIED="1622912299287" POSITION="left" TEXT="Constantes programa">
 <node CREATED="1622145534230" ID="ID_1453297853" MODIFIED="1622145540589" TEXT="Limites"/>
 <node CREATED="1622145550604" ID="ID_1178178824" MODIFIED="1622145558656" TEXT="Sprites"/>
 </node>
@@ -14,21 +14,109 @@
 </node>
 <node CREATED="1622145577653" ID="ID_1585760040" MODIFIED="1622488333258" POSITION="left" TEXT="Variables">
 <node CREATED="1622145585575" ID="ID_735791965" MODIFIED="1622145629505" TEXT="punto_mira"/>
-<node CREATED="1622145631148" ID="ID_810795369" MODIFIED="1622488301715" TEXT="n_reliquias"/>
 <node CREATED="1622145696235" ID="ID_342082184" MODIFIED="1622488291096" TEXT="n_enemigos"/>
-<node CREATED="1622145717734" ID="ID_439463766" MODIFIED="1622145725618" TEXT="matriz_fase">
-<node CREATED="1622488242278" ID="ID_481013700" MODIFIED="1622488270753" TEXT="x n&#xba; niveles (DW)"/>
-</node>
 <node CREATED="1622145737681" ID="ID_571800224" MODIFIED="1622145743260" TEXT="posicion_matriz">
 <node CREATED="1622488208608" ID="ID_52790538" MODIFIED="1622488235459" TEXT="un byte (0-7, 0-7)"/>
 </node>
-<node CREATED="1622145746998" ID="ID_537221061" MODIFIED="1622145749695" TEXT="vidas"/>
+<node CREATED="1622145746998" ID="ID_537221061" MODIFIED="1622912436858" TEXT="n_vidas">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      si hace falta se pueden unir las variables
+    </p>
+    <p>
+      n_vidas y n_reliquias en un byte
+    </p>
+    <p>
+      1111 (vidas 0-8) y 0000 reliquas (0-8)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1622145631148" ID="ID_810795369" MODIFIED="1622912441371" TEXT="n_reliquias">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      si hace falta se pueden unir las variables
+    </p>
+    <p>
+      n_vidas y n_reliquias en un byte
+    </p>
+    <p>
+      1111 (vidas 0-8) y 0000 reliquas (0-8)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 <node CREATED="1622145751512" ID="ID_1911242454" MODIFIED="1622145755171" TEXT="energia"/>
-<node CREATED="1622145768611" ID="ID_789611812" MODIFIED="1622145776603" TEXT="tiempo_salir"/>
 <node CREATED="1622488180299" ID="ID_1508061501" MODIFIED="1622488182804" TEXT="nivel"/>
 <node CREATED="1622488318931" ID="ID_320421743" MODIFIED="1622488322194" TEXT="arrays">
 <node CREATED="1622146240680" ID="ID_1431594522" MODIFIED="1622146256903" TEXT="array_sprites_pantalla"/>
-<node CREATED="1622488340578" ID="ID_1554144761" MODIFIED="1622488350799" TEXT="array_enemigos"/>
+<node CREATED="1622488340578" ID="ID_1554144761" MODIFIED="1622911933283" TEXT="array_enemigos_pantalla"/>
+<node CREATED="1622911805264" ID="ID_1421647868" MODIFIED="1622912696575" TEXT="array_niveles">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tipo DW
+    </p>
+    <p>
+      0 - lleno o vac&#237;o (si ya se ha pasado)
+    </p>
+    <p>
+      0000 - n&#186; puertas 1 arriba 1 derecha 1 abajo 1 izquierda
+    </p>
+    <p>
+      1 - hay reliquia?
+    </p>
+    <p>
+      1 - hay vida extra?
+    </p>
+    <p>
+      1 - hay arma extra (durar&#225; 6 pantallas - sonido distinto)
+    </p>
+    <p>
+      1 - hay energ&#237;a? (sobran bits ... se puede usar otro como carga energ&#237;a +)
+    </p>
+    <p>
+      1 - es pantalla final?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1622911829423" ID="ID_1886966464" MODIFIED="1622911842112" TEXT="array_nivel1"/>
+<node CREATED="1622911844095" ID="ID_364906659" MODIFIED="1622911846760" TEXT="array_nivel2"/>
+<node CREATED="1622911847673" ID="ID_1975537452" MODIFIED="1622911850650" TEXT="array_nivel3"/>
+<node CREATED="1622911856041" ID="ID_1480259804" MODIFIED="1622911858446" TEXT="array_nivel4"/>
+<node CREATED="1622911859748" ID="ID_721093526" MODIFIED="1622911862049" TEXT="array_nivel5"/>
+<node CREATED="1622911863125" ID="ID_340504692" MODIFIED="1622911865337" TEXT="array_nivel6"/>
+<node CREATED="1622911866969" ID="ID_1348790684" MODIFIED="1622911876514" TEXT="array_nivel7"/>
+</node>
+</node>
+<node CREATED="1622912759422" ID="ID_294820484" MODIFIED="1622912864429" TEXT="tiempo_salir">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      En caso de conseguir matar al jefe hay que salir en un tiempo
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1622912768584" ID="ID_428752042" MODIFIED="1622912771963" TEXT="minutos"/>
+<node CREATED="1622912773210" ID="ID_31313294" MODIFIED="1622912776821" TEXT="segundos"/>
 </node>
 </node>
 <node CREATED="1622145781074" HGAP="19" ID="ID_359756947" MODIFIED="1622487985197" POSITION="right" TEXT="inicializa_pantalla" VSHIFT="-28">
@@ -53,18 +141,7 @@
 </node>
 </node>
 <node CREATED="1622488375255" ID="ID_618587182" MODIFIED="1622488418724" TEXT="examina_matriz_fase_posicion">
-<node CREATED="1622145924766" ID="ID_1593287003" MODIFIED="1622488444623" TEXT="carga_fondo">
-<richcontent TYPE="NOTE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      crear 4 ejemplos
-    </p>
-  </body>
-</html>
-</richcontent>
+<node CREATED="1622145924766" ID="ID_1593287003" MODIFIED="1622912976735" TEXT="carga_fondo">
 <node CREATED="1622146176211" ID="ID_940567344" MODIFIED="1622146198112" TEXT="carga_patrones"/>
 <node CREATED="1622146184890" ID="ID_1057578605" MODIFIED="1622146190423" TEXT="carga_colores"/>
 <node CREATED="1622146201158" ID="ID_811163035" MODIFIED="1622146209176" TEXT="carga_mapa"/>
@@ -84,8 +161,7 @@
       hay que ver d&#243;nde van las reliquias, vida, etc...
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1622146405465" FOLDED="true" ID="ID_1500933088" MODIFIED="1622147246633" TEXT="mira_teclado_joystick">
 <node CREATED="1622146871854" ID="ID_1165765776" MODIFIED="1622146879453" TEXT="mira_posicion">
 <node CREATED="1622147111361" ID="ID_104666824" MODIFIED="1622147116486" TEXT="pos_arriba">
@@ -112,7 +188,7 @@
 <node CREATED="1622146590778" ID="ID_1050636519" MODIFIED="1622147210143" TEXT="las vidas y energ&#xed;a se actualizan aqu&#xed; o cuando toque?????"/>
 </node>
 </node>
-<node CREATED="1622145835192" FOLDED="true" ID="ID_361642530" MODIFIED="1622147257736" POSITION="right" TEXT="otras_funciones">
+<node CREATED="1622145835192" ID="ID_361642530" MODIFIED="1622912888746" POSITION="right" TEXT="otras_funciones">
 <node CREATED="1622145846435" ID="ID_1252974278" MODIFIED="1622145852729" TEXT="actualiza_vidas">
 <node CREATED="1622146097852" ID="ID_1406574668" MODIFIED="1622146105700" TEXT="pinta_vidas"/>
 </node>
