501e6bd6c28f1213bd7adeaadf754cf166c72f37
Paco Gálvez <correopaco2000@gmail.com>
2021-06-10 21:06:00 +0200

    actualizacion diseño

--- test1/src/pantallas.asm
+++ test1/src/pantallas.asm
@@ -1,6 +1,14 @@
 ;;=====================================================
 ;;DEFINICIÓN DE PANTALLAS
 ;;=====================================================		
-tiles_patrones_pantalla:	incbin "pan1_23.til.bank0.bin.chr"
-tiles_color_pantalla:		incbin "pan1_23.til.bank0.bin.clr"
-tiles_mapa_pantalla:		incbin "pan1_23.map.bank0.bin"
+tiles_patrones_pantalla11:	incbin "pan1_23.til.bank0.bin.chr"
+tiles_color_pantalla11:		incbin "pan1_23.til.bank0.bin.clr"
+tiles_mapa_pantalla11:		incbin "pan1_23.map.bank0.bin"
+
+tiles_patrones_pantalla12:	incbin "pan1_23.til.bank1.bin.chr"
+tiles_color_pantalla12:		incbin "pan1_23.til.bank2.bin.clr"
+tiles_mapa_pantalla12:		incbin "pan1_23.map.bank3.bin"
+
+tiles_patrones_pantalla13:	incbin "pan1_33.til.bank0.bin.chr"
+tiles_color_pantalla13:		incbin "pan1_33.til.bank0.bin.clr"
+tiles_mapa_pantalla13:		incbin "pan1_33.map.bank0.bin"
