d4a7e34fb5167fdc6bb880cab3cd61dfbc6eb785
Paco Gálvez <correopaco2000@gmail.com>
2021-09-03 11:24:01 +0200

    Copia de seguridad
    Para volver atrás más fácilmente con detalles concretos

---
+++ copias completas de seguridad/20210903 test1/src/t4.asm
@@ -0,0 +1,145 @@
+	OUTPUT	"t4.bin"
+	
+;;=====================================================
+;;DEFINICIÓN DE CONSTANTES
+;;=====================================================	
+	include "constantes.asm"
+
+;;=====================================================
+;;DEFINICIÓN DE CABECERA DE ARCHIVO BIN
+;;=====================================================		
+	include "cabecerabin.asm"
+	
+START:	
+	;inicializa pantalla y entonrno
+	CALL	sub_preparapantalla			;screen 2,2 sin click al pulsar tecla y color 16,1,1
+	
+	;inicializa variables para parametrizar funciones y que lo que se muestre sea variable (nº vidas, mapa, puertas, pantalla, etc...)
+	CALL	inicializa_variables_pruebas
+	
+	;inicializa los niveles por partida
+	CALL	inicializa_niveles			;no es necesario parametrizarlo según el nivel del usuario ya que se cargan todos
+	
+	;pinta la pantalla (la primera o algunas especiales se pintan completamente)
+	CALL	pinta_pantalla_completa 
+	
+	;carga sprites en memoria (SE HA RETRASADO HASTA PARAMETRIZAR LA CARGA DE PANTALLAS)
+	CALL 	carga_patrones_sprites_nivel
+	
+	;funciones que modificarian el marcador si se produce un evento cuando toque y cuando se pinta la pantalla ya se mira
+	;una variable para ver si el array hay que actualizarlo en pantalla o no
+	CALL	pinta_vidas
+	CALL 	pinta_reliquias
+	CALL	pinta_nivel
+	CALL	borra_mapa
+	CALL	pinta_puertas
+
+	LD		 A, TILEPROTAM
+	LD		(elemento_pintar_mapa), A
+	CALL	posiciona_en_mapa		;se le pasa elemento_pintar_mapa (tile del prota) está el prota y prota.poxx y posy
+	
+	;incializacion de replayer con interrupciones
+;	CALL	inicializa_replayer_efectos_interrupciones
+	
+loop_principal:
+	HALT							;espera VBLANK y sincroniza
+	
+	CALL	render_sprites			;actualiza array de sprites y los pinta en pantalla
+	
+	CALL	pinta_energia			;pinta la energia en pantalla
+
+	CALL	check_player			;MIRA EL CONTROL Y APLICA LA LOGICA DE MOVIMIENTO DEL PROTAGONISTA
+	
+	JP		loop_principal
+fin_programa_principal:
+	;RET
+
+
+
+
+
+;;*******************************************************************
+; sólo para hacer pruebas y pintar la parte de las vidas y demás
+inicializa_variables_pruebas:
+	LD		 A, 4
+	LD		(prota_vidas),A
+
+	LD		 A, 4
+	LD		(prota_reliquias), A
+
+	LD		 A, 100
+	LD		(prota_energia), A
+
+	;ubico al prota dentro del nivel para obtener luego las habitaciones y enemigos que aparecerán
+	;será igual la posición inicial en todos los niveles
+	
+	LD		 A, 4				;los niveles (matrices) son 7 del 0 al 6
+	LD		(prota_nivel), A	;nivel empieza en 0 para usar las posiciones ascii
+
+	LD		 A, 6				;los subniveles (filas) son 7 del 0 al 6
+	LD		(prota_pos_mapy), A	;pos Y dentro del nivel (se empieza en 0)
+
+	LD		 A, 2				;columnas 7: del 0 al 6
+	LD		(prota_pos_mapx), A	;pos X dentro del nivel (se empieza en 0)
+	
+	XOR		 A
+	LD		(prota.cadencia), A
+fin_inicializa_variables_pruebas:
+	RET
+;;************************************************************************
+
+
+
+
+
+
+
+
+
+;;=====================================================
+;;DEFINICIÓN DE SUBRUTINAS
+;;=====================================================
+	include "subrutinas.asm"
+	
+	include "subr_pintapantalla.asm"
+	
+	include "subr_sprites.asm"
+	
+	include "subr_teclado_joy.asm"
+	
+	include "subr_sonido.asm"
+	
+
+;;=====================================================
+;;DEFINICIÓN DE SUBRUTINAS DE FERNANDO PARA COMPRESIÓN Y SONIDO
+;;=====================================================
+;Este include lleva la rutina de descompresión de ROM/RAM a VRAM de pletter
+;Está adaptada de la original a sjasm
+;	HL = RAM/ROM source	; DE = VRAM destination
+depack_VRAM:
+	include "PL_VRAM_Depack_SJASM.asm"
+
+
+	
+;;=====================================================
+;;DEFINICIÓN DE ESTRUCTURAS
+;;=====================================================		
+	include "estructuras.asm"
+	
+;;=====================================================
+;;DEFINICIÓN DE VARIABLES
+;;=====================================================		
+	include "sprites.asm"
+	
+	include "habitaciones.asm"
+	
+	include "variables.asm"
+	
+;;=====================================================
+;;DEFINICIÓN DE PANTALLAS
+;;=====================================================		
+	include "pantallas.asm"
+
+
+	
+END:
