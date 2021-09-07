e3b508a59bd99746e5d5e6bdf936f981d4395e6d
Paco Gálvez <correopaco2000@gmail.com>
2021-09-07 20:04:33 +0200

    Copia de seguridad
    Antes de tocar funcion importante que funciona de pintar_array

---
+++ copias completas de seguridad/20210907 src/subrutinas.asm
@@ -0,0 +1,195 @@
+;;=====================================================
+;;DEFINICIÓN DE SUBRUTINAS
+;;=====================================================	
+
+
+;;=====================================================
+;;INICIALIZA_NIVELES
+;;=====================================================	
+; función: 	copia la plantilla de niveles (habitaciones_plantilla) en los niveles de trabajo para el juego (habitaciones_juego)
+; entrada: 	habitaciones_plantilla
+; salida: 	habitaciones_juego
+; toca: 	A, B, HL, DE
+inicializa_niveles:
+		XOR		 A
+		LD		 B,   49				;7 filas por 7 niveles menos el primero que lo hago fuera del buble
+		LD		HL,   habitaciones_juego;tambíen puntero a la primera fila del primer nivel
+		LD		DE,   14				;14 bytes de habitaciones y 2 extras
+		ADD		HL,   DE				;puntero al primero de los bytes extras
+		LD		(HL), A					;0000000Xb si X=0 no se ha pasado por allí y si es 1 sí
+		LD		DE,   16				;desde las col 15 de una fila a la siguiente
+.bucle_borra_hab:
+		ADD		HL,   DE				;situo el puntero 16 bytes siguientes
+		LD		(HL), A					;pongo su contenido a 0
+		DJNZ	.bucle_borra_hab		;repito para poner todos a 0
+fin_iniciliza_niveles:
+		RET
+
+
+;;=====================================================
+;;ACTUALIZA_MARCADOR_VIDAS
+;;=====================================================	
+; función:  actualiza el buffer de vidas
+; entrada: 	prota_vidas
+; salida: 	array_aux_vidas
+; toca:		A,B,HL
+actualiza_marcador_vidas:
+	;para no andar restando pongo todos los huecos a negro y luego en otro bucle las caras
+	;1-pinto todo en blanco
+	LD		HL,array_aux_vidas
+	LD		 B,NMAXVIDREL
+loop_marcador_caras_negro:	;asignar espacios en negro
+	LD		(HL),0			;0 posición mapa tiles para nada (es transparente pero el fondo es negro)
+	INC		HL
+	DJNZ	loop_marcador_caras_negro
+fin_loop_marcador_caras_negro:
+	;2-pinto las caras según el n de vidas
+	LD		 A, (prota_vidas)
+	OR		 A
+	RET 	 Z
+	LD		 B, A
+	LD		HL, array_aux_vidas
+loop_marcador_caras: ;asignar caras
+	LD		(HL), POSCARAMAP	;13 posición mapa tiles para cara
+	INC		HL
+	DJNZ	loop_marcador_caras
+fin_loop_marcador_caras:
+
+fin_actualiza_marcador_vidas:
+	RET
+
+
+;;=====================================================
+;;ACTUALIZA_BUFFER_RELIQUIAS
+;;=====================================================	
+; función: 	actualiza buffer de reliquias
+; entrada: 	prota_reliquias
+; salida: 	array_aux_reliquias
+; toca:		A,B,HL
+actualiza_buffer_reliquias:	
+	;para no andar restando pongo todos los huecos a negro y luego en otro bucle las reliquias
+	;1-pinto todo en blanco
+	LD		HL, array_aux_reliquias
+	LD		 B, NMAXVIDREL
+	XOR		 A
+loop_buffer_reliquias_negro:	;asignar espacios en negro
+	LD		(HL), A			;0 posición mapa tiles para nada (es transparente pero el fondo es negro)
+	INC		HL
+	DJNZ	loop_buffer_reliquias_negro
+
+	;2-pinto las cruces según el n de reliquias
+	LD		HL, array_aux_reliquias
+	LD		 A, (prota_reliquias)
+	OR		 A
+	RET		 Z
+	LD		 B, A
+loop_buffer_reliquias: ;asignar reliquias
+	LD		(HL), POSRELIMAP	;17 posición mapa tiles para reliquia
+	INC		HL
+	DJNZ	loop_buffer_reliquias
+fin_actualiza_buffer_reliquias:
+	RET
+
+
+
+;;=====================================================
+;;LOCALIZA_INFO_HABITACION
+;;=====================================================	
+; función: 	busca las puertas que hay que pintar egún la posición del usuario y las pone en habitacion_actual
+; entrada: 	prota.pos_mapy, prota.pos_mapx, prota_nivel habitaciones_juego
+; salida: 	habitacion_actual
+; toca:		todo
+;ejemplo: nivel 4, posy 5 y posx 3
+;3 + 5 x 8 + 4 x 49
+localiza_info_habitacion:
+;primera posición: me coloco en la columna correcta
+	LD		HL, habitaciones_juego
+.situo_columna:	
+	LD		 A, (prota_pos_mapx)
+	SLA		 A				;multiplico por dos ya que cada habitación va de dos en dos bytes
+	LD		 B, 0
+	LD		 C, A
+
+	ADD 	HL, BC			;sumo a la posición 0 la columnax2 en la que situarme
+	
+.situo_fila:
+	LD		A, (prota_pos_mapy)
+	OR	 	A
+	JP		Z, .situo_nivel	;si la fila es 0 no es necesario seguir operando en fila
+	
+	LD		B, A
+	XOR		A
+.loop_suma_fila:
+	ADD		16				;cada fila tiene 16 bytes
+	DJNZ	.loop_suma_fila
+	
+	LD		 B, 0
+	LD		 C, A
+
+	ADD 	HL, BC			;sumo a la columna 16 x filas para situarme en la fila correcta
+
+.situo_nivel:
+	LD		 A, (prota_nivel)
+	OR	 	 A
+	JP		 Z, .actualiza_valiable_habitacion	;si el nivel es 0 al pasar por las anterirores ya estoy situado actualizo la variable y termino
+	
+	PUSH	HL				;guardo el resultado intermedio
+	
+	LD		 B, A			;preparo la varible del bucle
+	LD		DE, 112			;112 = 16x42 son los bytes de cada matriz-nivel
+	LD		HL, 0
+.loop_suma_nivel:
+	ADD		HL, DE
+	DJNZ	.loop_suma_nivel
+	
+	POP		DE				;devuelvo el resultado intermedio en DE que ya no lo necesito
+	
+	ADD		HL, DE			;sumo el resultado del nivel al resultado intermedio anterior
+	
+.actualiza_valiable_habitacion
+	LD		 A, (HL)
+	LD		(habitacion_actual), A
+	INC		HL				;el byte de los extras está antes de donde se especifican las puertas
+	LD		 A, (HL)
+	LD		(habitacion_extras), A
+fin_localiza_info_habitacion:
+	RET
+
+
+
+;;=====================================================
+;;INICIALIZA AYUDAS
+;;=====================================================	
+; función: 	
+; entrada: 	
+; salida: 	-
+inicializa_ayudas:
+fin_inicializa_ayudas:
+	RET
+
+
+
+;;=====================================================
+;;ACTUALIZA_AYUDAS
+;;=====================================================	
+; función: 	
+; entrada: 	
+; salida: 	-
+actualiza_ayudas:
+	;busca objetos a incluir
+	LD		 A, (habitacion_extras)
+	
+	;recorre
+	;	pinta
+	;	actualiza estructura
+
+	LD		 A, AGUAOFF
+	CALL	pinta_obj_ayuda
+
+fin_actualiza_ayudas:
+	RET
+	
+	
+	
+	
+	
