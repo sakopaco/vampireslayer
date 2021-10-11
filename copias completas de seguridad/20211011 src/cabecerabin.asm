7f4b227549effec4565ce287d3c83ce72ad9dbfb
Paco Gálvez <correopaco2000@gmail.com>
2021-10-11 20:03:38 +0200

    Copia de seguridad
    porque voy a cambiar cómo se inicializan las estructuras

---
+++ copias completas de seguridad/20211011 src/cabecerabin.asm
@@ -0,0 +1,13 @@
+;;=====================================================
+;; FICHERO cabecera.asm
+;;=====================================================
+
+;;CABECERA FICHERO BIN
+	DB	#FE
+	DW	START
+	DW	END
+	DW	START
+
+;;INICIO CÓDIGO PROGRAMA	
+	ORG	#8000
+	
