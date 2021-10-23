84db1ead35221316f80acb293c4db870f0f24f53
Paco Gálvez <correopaco2000@gmail.com>
2021-10-23 20:52:16 +0200

    corrección

--- test1/src/subr_teclado_joy.asm
+++ test1/src/subr_teclado_joy.asm
@@ -118,15 +118,15 @@ mira_disparo:
 		CPL		 
 		LD		(prota.escena), A
 		;reseteo cadencia para el próximo disparo
-		LD		 A, LIMCADENCIA						;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		LD		 (prota.cadencia), A				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		;ejecuto sonido								;; ajustar cadencia y que cuando el disparo sea efectivo se reseteen los colores
+		LD		 A, LIMCADENCIA				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		LD		 (prota.cadencia), A		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		;ejecuto sonido						;; ajustar cadencia y que cuando el disparo sea efectivo se reseteen los colores
 		XOR		 A
 		LD		 C, 1
 		CALL	ayFX_INIT
 		
 
-		CALL	fin_check_colisiones_objetos		;revisa las colisiones con puertas, ayudas y enemigos
+		CALL	check_colisiones_objetos	;revisa las colisiones con puertas, ayudas y enemigos
 		
 		
 .fin_mira_disparo1
