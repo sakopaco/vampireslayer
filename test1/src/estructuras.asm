5397194970ca41664011c69d60176f893e407803
Paco Gálvez <correopaco2000@gmail.com>
2021-12-31 09:03:46 +0100

    Definiendo propiedades de enemigos
    Estructura y definiendo comportamiento

--- test1/src/estructuras.asm
+++ test1/src/estructuras.asm
@@ -82,7 +82,62 @@ ancho				DB		0	;ancho en tiles del dibujo de la puerta (columnas)
 	ENDSTRUCT;ESTRUCTURA_PUERTA
 
 
+;Tipo:
+;		0 - inactivo
+;		1 - muerto (se pone daño 0 y vida: el tiempo apra desaparecer)
+;movimiento:
+;	1 - ciempies ????? (quieto)
+;	2 - murciélago
+;		araña 
+;	"precalculadas" de 8 posibles (para usar R) desde la parte superior e irán bajando y 
+;	subiendo verticalmente y con pos aleatorias horizontales para que no vayan todos al unísono
+;	3 - lobo
+;		zombie
+;		cabalero blanco y negro
+;	"precalculadas" de 8 posibles (para usar R) desde la parte izquierda a la derecha moviéndose
+;	horizontalmente y con pos aleatorias verticales para que no vayan todos al unísono
+;	4 - fantasma
+;		fuego
+;	describe círculos desde un punto central con radios (pseudoaleatorios) a cada vuelta
+;	5 - magia
+;		posiciones "pseudoaleatorias" por pantalla estando un contador en cada posición
 
 
+
+	STRUCT ESTRUCTURA_ENEMIGO
+activo_tipo			DB		0	;si inactivo = 0 si <> 0 es el tipo de enemigo
+escena				DB		0	;sprite a mostrar
+cont_sig_escena		DB		0	;siguiente sprite a mostrar ??(como normalmente usaré 2 no se si esto se usará)
+reset_contador		DB		0	;se usa constante
+ptr_sig_escena		DW		0	;?? si hay dos escenas no tiene sentido
+energia				DB		0	;energía del enemigo antes de morir
+direccion			DB		0	;?? (duda por si no tendría espacio para tanto sprite en memoria)
+pos_x				DB		0	;pos x para mover y punto central del sprite para revisar disparo
+pos_y				DB		0	;pos y para mover y punto central del sprite para revisar disparo
+inc_x				DB		0	;incremento x para mover
+inx_y				DB		0	;incremento y para mover
+ptr_mover			DW		0	;puntero a subrutina que moverá el enemigo según el tipo de enemigo (se pasa al inicializar)
+;se pondrán 8 sprites: 4 por si hay enemigos de hasta 4 sprites y por 2 por usar 2 escenas... si se usan 3 habría que ampliar
+sprite_1a			DB		0
+sprite_2a			DB		0
+sprite_3a			DB		0
+sprite_4a			DB		0
+sprite_1b			DB		0
+sprite_2b			DB		0
+sprite_3b			DB		0
+sprite_4b			DB		0
+sprite_1c			DB		0
+sprite_2c			DB		0
+sprite_3c			DB		0
+sprite_4c			DB		0
+sprite_1d			DB		0
+sprite_2d			DB		0
+sprite_3d			DB		0
+sprite_4d			DB		0
+ptr_explosion		DW		0	;realiza cambios en caso de muerte
+	ENDSTRUCT;ESTRUCTURA_ENEMIGO
+
+array_dano:
+		DB			
 
 
