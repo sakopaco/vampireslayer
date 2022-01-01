13b29a2ef6722d014770ffef29a0b48d17164ebe
Paco Gálvez <correopaco2000@gmail.com>
2022-01-01 12:44:03 +0100

    Se termina de definir estructura*/movimiento enemigos
    Faltaría depurar y comenzar pero en principio ya es rellenar estructura
    * Importante: falta ver como volcar datos en array de sprites y si se almacenará en estructura la pos dentro del array

--- test1/src/estructuras.asm
+++ test1/src/estructuras.asm
@@ -35,24 +35,6 @@ cont_sig_escena		DB		0	;	6
 	ENDSTRUCT;ESTRUCTURA_ESQUELETO
 	
 
-	STRUCT ESTRUCTURA_ENEMIGO
-tipo				DB		0	;(0 serpiente por ejemplo) #FF muerto o no existe
-posx				DB		5
-posy				DB		50
-color				DB		5
-escena				DB		0
-contador_siguiente_escena	DB	0
-reset_contador		DB		0
-dano				DB		0	;daño
-energia				DB		0	;energía del enemigo... si llega a 0 => existe <- 0
-;indica el nº de sprite a mostrar y si está compuesto por uno o dos sprites y si están en horizontal o vertical
-ver_hor				DB		0
-manejador_dibujo 	DW		0
-en_aux3				DB		0
-en_aux4				DB		0
-	ENDSTRUCT;ESTRUCTURA_ENEMIGO
-
-
 	STRUCT ESTRUCTURA_AYUDA	
 activa				DB		0	;0 no activa <>0 activo (y muestra tiles ayudaoff)
 posx				DB		0	;punto x de la ayuda para cuando se dispare encima
@@ -83,39 +65,66 @@ ancho				DB		0	;ancho en tiles del dibujo de la puerta (columnas)
 
 
 ;Tipo:
-;		0 - inactivo
-;		1 - muerto (se pone daño 0 y vida: el tiempo apra desaparecer)
+;	0  - inactivo
+;	1  - muerto (se pone daño 0 y vida: el tiempo para desaparecer)
+;	2  - ciempies
+;	3  - serpiente
+;	4  - araña
+;	5  - murciélago
+;	6  - lobo
+;	7  - zombi
+;	8  - fantasma
+;	9  - caballero gris
+;	10 - caballero negro
+;	11 - fuego
+;	12 - magia
+;	13 - jefe lobo gigante
+;	14 - jefe murciélago gigante
+;	15 - jefe fantasma gigante
+;	16 - jefe zombie gigante
+;	17 - jefe caballero
+;	18 - jefe beholder
+;	19 - manos conde drácula
+;	20 - conde drácula
+;		
 ;movimiento:
-;	1 - ciempies ????? (quieto)
+;	1 - ciempies
+;		beholder
+;		posiciones aleatoriaas precalculadas (duran en posición cont_sig_escena)
 ;	2 - murciélago
 ;		araña 
-;	"precalculadas" de 8 posibles (para usar R) desde la parte superior e irán bajando y 
-;	subiendo verticalmente y con pos aleatorias horizontales para que no vayan todos al unísono
-;	3 - lobo
+;		"precalculadas en horizontal" de 8 posibles (para usar R) desde la parte superior e irán bajando y 
+;		subiendo verticalmente y con pos aleatorias horizontales para que no vayan todos al unísono
+;		NOTA: para el murciélado me gustaría una onda senoidal a la altura de un tercio 				?? investigar
+;	3 - serpiente
+;		lobo
 ;		zombie
-;		cabalero blanco y negro
-;	"precalculadas" de 8 posibles (para usar R) desde la parte izquierda a la derecha moviéndose
-;	horizontalmente y con pos aleatorias verticales para que no vayan todos al unísono
+;		cabalero gris y negro
+;		"precalculadas en vertical" de 8 posibles (para usar R) desde la parte izquierda a la derecha moviéndose
+;		horizontalmente y con pos aleatorias verticales para que no vayan todos al unísono
 ;	4 - fantasma
 ;		fuego
-;	describe círculos desde un punto central con radios (pseudoaleatorios) a cada vuelta
+;		describe círculos desde un punto central con radios (pseudoaleatorios) a cada vuelta
 ;	5 - magia
+;		manos del conde drácula
 ;		posiciones "pseudoaleatorias" por pantalla estando un contador en cada posición
-
+;	6 - conde drácula
+;		posiciones aleatoriaas precalculadas (duran en posición cont_sig_escena)
 
 
 	STRUCT ESTRUCTURA_ENEMIGO
 activo_tipo			DB		0	;si inactivo = 0 si <> 0 es el tipo de enemigo
-escena				DB		0	;sprite a mostrar
-cont_sig_escena		DB		0	;siguiente sprite a mostrar ??(como normalmente usaré 2 no se si esto se usará)
-reset_contador		DB		0	;se usa constante
-ptr_sig_escena		DW		0	;?? si hay dos escenas no tiene sentido
+escena				DB		0	;sprite a mostrar 1/2
+cont_sig_escena		DB		0	;retardo_explosion ;contador para ver cuando cambiar de sprite (y retardo_explosión irá hasta cero antes de que desaparezca la explosión)
+tiemp_cam_escena	DB		0	;cada cuantas iteraciones se cambiará la escena
+ptr_sig_escena		DW		0	;funcion que cambia los sprites de la escena (según el nº de sprites será distinta)
 energia				DB		0	;energía del enemigo antes de morir
-direccion			DB		0	;?? (duda por si no tendría espacio para tanto sprite en memoria)
+ptr_explosion		DW		0	;realiza cambios en caso de muerte (energía = 0)
 pos_x				DB		0	;pos x para mover y punto central del sprite para revisar disparo
 pos_y				DB		0	;pos y para mover y punto central del sprite para revisar disparo
 inc_x				DB		0	;incremento x para mover
 inx_y				DB		0	;incremento y para mover
+direccion			DB		0	;?? (duda por si no tendría espacio para tanto sprite en memoria)
 ptr_mover			DW		0	;puntero a subrutina que moverá el enemigo según el tipo de enemigo (se pasa al inicializar)
 ;se pondrán 8 sprites: 4 por si hay enemigos de hasta 4 sprites y por 2 por usar 2 escenas... si se usan 3 habría que ampliar
 sprite_1a			DB		0
@@ -134,10 +143,13 @@ sprite_1d			DB		0
 sprite_2d			DB		0
 sprite_3d			DB		0
 sprite_4d			DB		0
-ptr_explosion		DW		0	;realiza cambios en caso de muerte
+color1				DB		0	;color sprite 1
+color2				DB		0	;color sprite 1
+color3				DB		0	;color sprite 1
+color4				DB		0	;color sprite 1
 	ENDSTRUCT;ESTRUCTURA_ENEMIGO
 
 array_dano:
-		DB			
+		DB			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
 
