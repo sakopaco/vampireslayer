df6378c0a60860d37fe77cfdd7af1b50c762733a
Paco Gálvez <correopaco2000@gmail.com>
2022-02-26 13:38:06 +0100

    Copia completa de seguridad

---
+++ copias completas de seguridad/20220226 src/constantesenemigos.asm
@@ -0,0 +1,8 @@
+;;=====================================================
+;; FICHERO constantesenemigos.asm
+;;=====================================================
+
+;;ETIQUETAS/CONSTANTES CIENPIES
+CIENPIES_COLOR		equ		#000C	;12 - VERDE OSCURO
+CIENPIES_SPRITE1A	equ		#0018	;24
+CIENPIES_SPRITE2A	equ		#001c	;28
