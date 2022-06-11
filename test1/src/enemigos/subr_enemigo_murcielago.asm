ddd5171bb3e346a33e26a4378b3b9bcae89129c8
Paco Gálvez <correopaco2000@gmail.com>
2022-06-11 20:33:40 +0200

    Depurado murciélago
    Se mejora el movimiento y nos ahorramos el array y se elimina el bug.

--- test1/src/enemigos/subr_enemigo_murcielago.asm
+++ test1/src/enemigos/subr_enemigo_murcielago.asm
@@ -1,30 +1,20 @@
-
-
-
-;depurar movimiento de murciélago
-
-
-
 ;;=====================================================
 ;;CONTANTES MURCIELAGO
 ;;=====================================================
-variacion_murcielagoy:
-			DB		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
-
 datos_murcielago:
 			DB		5		;(activo_tipo) si inactivo = 0 si <> 0 es el tipo de enemigo
 			DB		0		;(escena) sprite a mostrar 1/2
-			DB		00010000b		;(cont_sig_escena) retardo_explosion ;contador para ver cuando cambiar de sprite (y retardo_explosión irá hasta cero antes de que desaparezca la explosión)
+			DB		00010000b			;(cont_sig_escena) retardo_explosion ;contador para ver cuando cambiar de sprite (y retardo_explosión irá hasta cero antes de que desaparezca la explosión)
 			DB		10		;(energia) energía del enemigo antes de morir
-			DB		MURCIELAGO_LIMIZQ	;(posx) pos x para mover y punto central del sprite para revisar disparo
-			DB		24		;(posy) pos y para mover y punto central del sprite para revisar disparo
+			DB		16		;(posx) pos x para mover y punto central del sprite para revisar disparo
+			DB		MURCIELAGO_HORIZON	;(posy) pos y para mover y punto central del sprite para revisar disparo
 			DB		8		;(radiox) radio x del enemigo para cuando se dispare encima
 			DB		8		;(radioy) radio y del enemigo para cuando se dispare encima
 			DB		0		;(incx) incremento x para mover
 			DB		0		;(inxy) incremento y para mover
 			DB		DIRDERECHA			;(direccionx) 0 derecha <> 0 izquierda // 0 abajo <> 0 arriba
 			DB		0		;(direcciony) 0 derecha <> 0 izquierda // 0 abajo <> 0 arriba
-			DB		0		;(pasos) pasos para no comprobar los límites de pentalla, sólo si pasos ha llegado a 0
+			DB		MURCIELAGO_PASOS	;(pasos) pasos para no comprobar los límites de pentalla, sólo si pasos ha llegado a 0
 			DB		0		;(radio) radio para movimientos circulares
 			DW		mover_murcielago	;(ptr_mover) puntero a subrutina que moverá el enemigo según el tipo de enemigo (se pasa al inicializar)
 			DB		0		;izq arriba
@@ -58,18 +48,9 @@ fin_anade_enemigo_murcielago:
 ;;=====================================================	
 ; función: 	inicializa valores aleatorios del murciélago
 ; entrada:	IX que equivaldrá a qué nº de enemigo estamos inicializando (por ejemplo enemigo1)
-; salida: 	posicion_anterior_arana
+; salida: 	-
 ; toca:		-
 actualiza_valores_murcielago:
-;actualiza_valores_aleatorios_murcielago
-.asigna_valores_posicion_x:
-		LD			 A, R
-		AND			01111111b
-		ADD			MURCIELAGO_LIMIZQ
-		LD			(IX + ESTRUCTURA_ENEMIGO.posx), A
-		
-.asigna_valores_posicion_y:
-		LD			(IX + ESTRUCTURA_ENEMIGO.posy), MURCIELAGO_HORIZON
 fin_actualiza_valores_murcielago:
 		RET
 		
@@ -124,25 +105,24 @@ fin_calcula_murcielago_escena:
 
 
 ;;=====================================================
-;;CALCULA_ARANA_INCREMENTOY
+;;CALCULA_MURCIELAGO_INCREMENTOY
 ;;=====================================================	
 calcula_murcielago_incrementoy:
-		LD			HL, variacion_murcielagoy
-		
-		LD			 A, (IX + ESTRUCTURA_ENEMIGO.posx)
-		AND			00011111b
-		CALL		suma_A_HL
-		
-		LD			 B, (IX + ESTRUCTURA_ENEMIGO.posy)
-		LD			 A, (HL)
-		ADD			 B
-		LD			(IX + ESTRUCTURA_ENEMIGO.posy), A
-fin_calcula_murcielago_incrementoy:
+		LD			 A, (IX + ESTRUCTURA_ENEMIGO.pasos)
+		AND			00100000b
+		JP			NZ, .baja
+.sube:
+		DEC			(IX + ESTRUCTURA_ENEMIGO.posy)
 		RET
+.baja:
+		INC			(IX + ESTRUCTURA_ENEMIGO.posy)
+		RET
+fin_calcula_murcielago_incrementoy:
+
 		
 
 ;;=====================================================
-;;CALCULA_ARANA_INCREMENTOx
+;;CALCULA_MURCIELAGO_INCREMENTOx
 ;;=====================================================	
 calcula_murcielago_incrementox:
 		LD			 A, (IX + ESTRUCTURA_ENEMIGO.direccionx)
@@ -153,17 +133,18 @@ calcula_murcielago_incrementox:
 			JP			.fin_mueve
 .mueve_derecha:
 			INC			(IX + ESTRUCTURA_ENEMIGO.posx)
-.fin_mueve:		
-		LD			(IX + ESTRUCTURA_ENEMIGO.posx), A
+.fin_mueve:
 
-		CP			MURCIELAGO_LIMIZQ
-		JP			NZ, .no_gira_derecha
-			LD			(IX + ESTRUCTURA_ENEMIGO.direccionx), DIRDERECHA
-
-.no_gira_derecha:
-		CP			MURCIELAGO_LIMDER
-		JP			NZ, .no_gira_izquierda
-			LD			(IX + ESTRUCTURA_ENEMIGO.direccionx), DIRIZQUIERDA
-.no_gira_izquierda:
+		;mira si cambia sentido
+		DEC			(IX + ESTRUCTURA_ENEMIGO.pasos)
+		LD			 A, (IX + ESTRUCTURA_ENEMIGO.pasos)
+		OR			 A
+		RET			NZ
+			;cambia sentido
+			LD			 A, (IX + ESTRUCTURA_ENEMIGO.direccionx)
+			XOR			 00000001b
+			LD			(IX + ESTRUCTURA_ENEMIGO.direccionx), A
+			LD			(IX + ESTRUCTURA_ENEMIGO.pasos), MURCIELAGO_PASOS
+			LD			(IX + ESTRUCTURA_ENEMIGO.posy), MURCIELAGO_HORIZON
 fin_calcula_murcielago_incrementox:
 		RET
