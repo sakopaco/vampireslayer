9046abd4e4ac8aef5bffd5923c85350b6faf3e73
Paco Gálvez <correopaco2000@gmail.com>
2022-06-20 19:47:18 +0200

    Copia completa de seguridad

--- copias completas de seguridad/20220417 src/ejecutar.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-sjasm -s variables.asm
-
-sjasm -s vs.asm
-
--- copias completas de seguridad/20220501 src/compilar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sjasm -s ./variables.asm variables.dat
-
-sjasm -s ./vs.asm
-
-rm -f ../bin/vs.rom
-
-mv vs.rom ../bin/.
