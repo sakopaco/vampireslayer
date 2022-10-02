2f3ad6110e0980ea3ae518b9cc5211fe691a92f0
Paco Gálvez <correopaco2000@gmail.com>
2022-10-02 09:57:58 +0200

    Se prueba una nueva música ingame
    Me parece genial aunque el tamaño me preocupa

--- test1/src/sonido/subr_sonido.asm
+++ test1/src/sonido/subr_sonido.asm
@@ -64,7 +64,7 @@ subrutinas_sonido:
 ;;=====================================================		
 song:
 	;incbin "bloodytears_castlevania.99"
-	incbin "ingame.99"
+	incbin "ingame5.99"
 	
 sfx_bank:
 	incbin "demo.afb"
